fd51f37b6f9a1953814ec93be68539c0eb0ac070
Nikita Efremov <heknt90@mail.ru>
2021-02-21 15:58:05 +0300

    тестирование работы с Firefox браузером

---
+++ selenium_course/3/test_gecko.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
+driver = webdriver.Firefox()
+
+driver.get("https://stepik.org/lesson/25969/step/8")
+
+time.sleep(3)
+
+driver.quit()
